0dbaf992f5b176294e5f4c59a1d48261301c1811
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-14 12:08:20 +0800

    Updated build and test procedure.

--- test.js
+++ test.js
@@ -66,9 +66,30 @@ const realign = require( "./realign.js" );
 
 describe( "realign", ( ) => {
 
-} );
-//: @end-server
+	describe( "`realign with multiline string`", ( ) => {
 
+		it( "should re-align whitespaces and tabs in a multi-line string", ( ) => {
+
+			let lipsum = realign(
+				`Lorem Ipsum is simply dummy text of the printing
+				and typesetting industry. Lorem Ipsum has been
+				the industry's standard dummy text ever since
+				the 1500s, when an unknown printer took a
+				galley of type and scrambled it to make a type
+				specimen book. It has survived not only five
+				centuries, but also the leap into electronic
+				typesetting, remaining essentially unchanged`
+			);
+
+			assert.ok( lipsum );
+
+		} );
+
+	} );
+
+} );
+
+//: @end-server
 
 
 
